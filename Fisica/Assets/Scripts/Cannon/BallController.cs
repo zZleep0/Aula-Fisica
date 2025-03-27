a4173cec9a840fba7f511fc7fa4226bc731494e2
zZleep0 <152449401+zZleep0@users.noreply.github.com>
2025-03-26 22:04:40 -0300

    particulas

--- Fisica/Assets/Scripts/Cannon/BallController.cs
+++ Fisica/Assets/Scripts/Cannon/BallController.cs
@@ -4,12 +4,17 @@ public class BallController : MonoBehaviour
 {
     public WinCondition winCondition;
 
+    public ParticleSystem explosionParticleSystem;
+    public float radius = 5f;
+    public float explosionForce = 700f;
+
     private void Start()
     {
         winCondition = GameObject.Find("Cannon").GetComponent<WinCondition>();
         Destroy(gameObject, 3f);
     }
 
+    
     private void OnCollisionEnter(Collision collision)
     {
         if (collision.collider.CompareTag("Alvo"))
@@ -17,10 +22,24 @@ public class BallController : MonoBehaviour
             winCondition.pontuacao++;
             Destroy(collision.gameObject);
         }
-        
 
-        Debug.Log($"Objeto {gameObject} colidiu com {collision.gameObject}");
+        Instantiate(explosionParticleSystem, transform.position, transform.rotation);
 
+        Collider[] colliders = Physics.OverlapSphere(transform.position, radius);
+
+        foreach(Collider nearbyObjectCollider in colliders)
+        {
+            Rigidbody rigidbody = nearbyObjectCollider.GetComponent<Rigidbody>();
+
+            if (rigidbody != null && (rigidbody != gameObject.GetComponent<Rigidbody>()))
+            {
+                rigidbody.AddExplosionForce(explosionForce, transform.position, radius, explosionForce);
+            }
+        }
+
+        Destroy(gameObject);
+
+        //Debug.Log($"Objeto {gameObject} colidiu com {collision.gameObject}");
         
     }
 }
